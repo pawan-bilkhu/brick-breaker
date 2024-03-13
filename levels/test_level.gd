df1fb83726491f3ba862e62e43d9f380994f6ded
Pawan <bilkhu.pawan@gmail.com>
2024-03-12 17:03:42 -0700

    Refactored powerupbase class and scene, removed the need for a staticbody object, updated game manager to include a create ball function, added a signal to detect when a ball has left the screen, changed power up to emit signal once the paddle has collided with it

--- levels/test_level.gd
+++ levels/test_level.gd
@@ -5,13 +5,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GameManager.ball_destroyed.connect(create_ball)
 	GameManager.create_object(ball_spawn.position, GameManager.OBJECTS.BALL)
-	GameManager.create_bricks(7, 10, Vector2(150,100), 110, 30)
+	GameManager.create_bricks(10, 10, Vector2(150,100), 110, 30)
 	GameManager.create_object(paddle_spawn.position, GameManager.OBJECTS.PADDLE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func create_ball() -> void:
+	GameManager.create_object(ball_spawn.position, GameManager.OBJECTS.BALL)
 
 	
